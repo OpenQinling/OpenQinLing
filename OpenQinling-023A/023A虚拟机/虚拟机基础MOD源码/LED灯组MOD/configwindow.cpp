c6effad833f66776afe61e159e4755aff25d75b2
enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>
2024-04-06 11:15:59 +0000

    设备MOD源码
    Signed-off-by: enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>

---
+++ OpenQinling-023A/023A虚拟机/虚拟机基础MOD源码/LED灯组MOD/configwindow.cpp
@@ -0,0 +1,25 @@
+﻿#include "configwindow.h"
+#include "ui_configwindow.h"
+
+ConfigWindow::ConfigWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ConfigWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton,&QPushButton::clicked,[=](){
+        this->close();
+    });
+}
+
+ConfigWindow::~ConfigWindow()
+{
+    delete ui;
+}
+
+void ConfigWindow::closeEvent(QCloseEvent *){
+    r = ui->spinBox_R->value();
+    g = ui->spinBox_G->value();
+    b = ui->spinBox_B->value();
+    count = ui->spinBox_COUNT->value();
+}
