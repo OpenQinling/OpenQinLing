83ca8e1db1691061490dedc514084b1b835f5dc7
enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>
2024-04-06 11:15:35 +0000

    设备MOD源码
    Signed-off-by: enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>

---
+++ OpenQinling-023A/023A虚拟机/虚拟机基础MOD源码/显示设备MOD/systemdependfunction.cpp
@@ -0,0 +1,14 @@
+﻿#include "systemdependfunction.h"
+#include <windows.h>
+
+void SystemDependFunction::setMousePos(int x,int y){
+    SetCursorPos(x,y);
+}
+
+void SystemDependFunction::getMousePos(int&x,int&y){
+    POINT point;
+    GetCursorPos(&point);
+    x = point.x;
+    y = point.y;
+    ShowCursor(false);
+}
