972fe64fb21a60367464f347f6e257fc3a75a9ac
enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>
2024-06-19 01:58:10 +0000

    C语言编译器完成
    Signed-off-by: enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>

---
+++ OpenQinling-023A/023A编译工具链/编译工具链源码/OpenQinling_ASM_FilterMemBlock.cpp
@@ -0,0 +1,71 @@
+﻿#include "OpenQinling_ASM_FilterMemBlock.h"
+#pragma execution_character_set("utf-8")
+QT_BEGIN_NAMESPACE
+namespace OpenQinling {
+namespace ASM_Compiler{
+
+
+//根据名称获取内存块索引号
+IndexV2 getBlockIndexFromName(QList<AsmSrc> &src,QString &name){
+    for(int i = 0;i<src.length();i++){
+        for(int j =0;j<src[i].memBlocks.length();j++){
+            if(src[i].memBlocks[j].blockName==name){
+                return IndexV2({i,j});
+            }
+        }
+    }
+    return IndexV2();
+}
+
+//查询allBlocks中有用的内存块
+void inquireUsefulBlocks(MarksBlockNames &nodeUseBlock,QHash<QString,MarksBlockNames> &range,QStringList &usefulBlockNames){
+
+    for(int i = 0;i<nodeUseBlock.length();i++){
+        if(range.contains(nodeUseBlock[i]) && !usefulBlockNames.contains(nodeUseBlock[i])){
+            usefulBlockNames.append(nodeUseBlock[i]);
+            inquireUsefulBlocks(range[nodeUseBlock[i]],range,usefulBlockNames);
+        }
+    }
+}
+
+//过滤掉无用的内存块
+void filtrMemBlock(QList<AsmSrc> &asmSrcList){
+    QHash<QString,MarksBlockNames> noExportBlockMarkInfo;//非导出内存块，标记引用的信息
+    MarksBlockNames exportBlockMarkInfos;//导出内存块，标记引用的信息
+    for(int i = 0;i<asmSrcList.length();i++){
+        for(int j = 0;j<asmSrcList[i].memBlocks.length();j++){
+            AsmMemBlock & thisBlock = asmSrcList[i].memBlocks[j];
+
+            QStringList blockNames;
+            for(int k = 0;k<thisBlock.bin.markQuoteList.length();k++){
+                blockNames.append(thisBlock.bin.markQuoteList[k].affiBlockName);
+            }
+            if(thisBlock.isExport){
+                exportBlockMarkInfos.append(blockNames);
+            }else{
+                noExportBlockMarkInfo.insert(thisBlock.blockName,blockNames);
+            }
+        }
+    }
+
+    //已各个导出内存块为根节点，解析出所有使用了的非导出内存块
+    QStringList usefulBlockNames;
+    inquireUsefulBlocks(exportBlockMarkInfos,noExportBlockMarkInfo,usefulBlockNames);
+    //获取要去除的内存块名称
+    QStringList uselessBlockNames;
+    QStringList allBlockNames = noExportBlockMarkInfo.keys();
+    for(int i = 0;i<allBlockNames.length();i++){
+        if(!usefulBlockNames.contains(allBlockNames[i])){
+            uselessBlockNames.append(allBlockNames[i]);
+        }
+    }
+    foreach(QString blockName,uselessBlockNames){
+        IndexV2 index = getBlockIndexFromName(asmSrcList,blockName);
+        asmSrcList[index.index1].memBlocks.removeAt(index.index2);
+    }
+}
+
+
+
+}}
+QT_END_NAMESPACE
---
+++ OpenQinling-023A/023A编译工具链/编译工具链源码/OpenQinling_ASM_TextFlag.h
@@ -0,0 +1,38 @@
+﻿#ifndef OPENQINLING_ASM_TEXTFLAG_H
+#define OPENQINLING_ASM_TEXTFLAG_H
+#include <QString>
+#include <QStringList>
+#include <QDebug>
+QT_BEGIN_NAMESPACE
+namespace OpenQinling {
+namespace ASM_Compiler{
+
+//字符串提取器，并将原字符串的位置替换为标志号,标志号对应一个被提取的字符串，用于还原
+//标志号的结构: $类型码 索引码 [类型为2位16进制数，索引号为5位16进制数]
+//示例:   $01100a1  01是字符串的类型码，100a1对应字符串缓存库中第0x100a1位置的字符串
+//isWindup是否去除成功了(如果不成功，说明存在一个字符串没有收尾) strStartp没有收尾的那个字符串定义的索引号
+QString extractString(QString txt,QStringList* bank,bool*isWindup,int*strStartp=NULL);
+
+//括号提取器[type:括号类型  0() 1[] 2{} 3<>]
+//$1type 5位索引码[示例 $1100a1 11是[]括号的类型码]
+QString extractParent(QString txt,uint type,QStringList* bank);
+
+//获取一段字符串中，存在多少个指定类型的标志号
+uint getFlagQuantity(QString text,QString typeCode);
+
+//从标志号中提取出字符串
+QString getStringFromFlag(QString flagTXT,QString typeCode,QStringList &str,bool*isSrceess=NULL);
+
+//将一串文本中，所有标志号替换为原先的字符串
+QString reductionString(QString text,QString typeCode,QStringList &str);
+
+//判断一段文本是否是一个标志号
+bool textIsFlag(QString text);
+
+//从一段文本中分离出普通字符串和标志号(已$号进行分割)
+QStringList separateFlag(QString text);
+
+}}
+QT_END_NAMESPACE
+
+#endif // OPENQINLING_ASM_TEXTFLAG_H
