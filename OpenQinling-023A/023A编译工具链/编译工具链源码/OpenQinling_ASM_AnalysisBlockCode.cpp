230dd2f78d6bcdfa1e8e481af4097215cbf31079
enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>
2024-06-19 01:57:37 +0000

    C语言编译器完成
    Signed-off-by: enlighten7328 <13295999+enlighten7328@user.noreply.gitee.com>

---
+++ OpenQinling-023A/023A编译工具链/编译工具链源码/OpenQinling_ASM_AnalysisBlockCode.cpp
@@ -0,0 +1,161 @@
+﻿#include "OpenQinling_ASM_AnalysisBlockCode.h"
+#include "OpenQinling_FormatText.h"
+#pragma execution_character_set("utf-8")
+QT_BEGIN_NAMESPACE
+namespace OpenQinling {
+namespace ASM_Compiler{
+
+//解析源码文件定义的内存块中的指令
+bool analysisAsmBlockCode(AsmSrc &asmSrc,QStringList*promptText){
+    if(promptText==NULL)return false;
+    QList<Prompt> errorPrompt,warnPrompt;
+    //errorPrompt会造成程序强制退出。warnPrompt则不会退出编译
+    for(int i = 0;i<asmSrc.memBlocks.length();i++){
+        QList<uint> lineFeed,sLine;//各指令原先文本的换行数
+        QStringList orderStrList = segment(asmSrc.memBlocks[i].blockSrcText,&lineFeed,&sLine);//换行符转为空格，并以';'号分割程序文本
+        QList<uint> startLine,endLine;//各指令的起始行数和结束行数
+
+        for(int j = 0;j<orderStrList.length();j++){
+            QList<int> line = getBlockPromptLine(j,asmSrc.memBlocks[i].blockStartLine,lineFeed,sLine);
+            startLine.append(line[0]);
+            endLine.append(line[1]);
+        }
+
+        //去除因换行符产生的多余空格
+        for(int j = 0;j<orderStrList.length();j++){
+            orderStrList[j] = dislodgeSpace(orderStrList[j]);
+        }
+
+        //判断最后一个定义的指令是否有分号结尾[如无就报错退出]
+        if(orderStrList[orderStrList.length()-1] != ""){
+            Prompt prpt;
+            prpt.isError = 1;
+            prpt.content = "指令格式错误";
+            prpt.append(asmSrc.path,asmSrc.asmSrcText,startLine[orderStrList.length()-1],endLine[orderStrList.length()-1]);
+            errorPrompt.append(prpt);
+            continue;
+        }else{
+            orderStrList.removeLast();
+            startLine.removeLast();
+            endLine.removeLast();
+        }
+
+
+        for(int j = 0;j<orderStrList.length();j++){
+            int formatStatus;
+
+            AsmBlockCode code = formatBlockOrderText(orderStrList[j],&formatStatus);
+            code.startLine = startLine[j];
+            code.endLine = endLine[j];
+            if(formatStatus){
+                qDebug()<<orderStrList[j];
+                Prompt prpt;
+                prpt.isError = 1;
+                if(formatStatus==1){
+                    prpt.content = "标记名称为空";
+                }else if(formatStatus==2){
+                    prpt.content = "标记名中不得存在空格";
+                }else if(formatStatus==3){
+                    prpt.content = "指令语法格式不正确";
+                }
+                prpt.append(asmSrc.path,asmSrc.asmSrcText,startLine[j],endLine[j]);
+                errorPrompt.append(prpt);
+                continue;
+            }
+
+            asmSrc.memBlocks[i].codes.append(code);
+        }
+    }
+
+    foreach(Prompt error,errorPrompt){
+        promptText->append(grenratePromptText(error));
+    }
+
+    foreach(Prompt warn,warnPrompt){
+        promptText->append(grenratePromptText(warn));
+    }
+
+    //存在错误的情况
+    if(errorPrompt.length()>0){
+        return false;
+    }
+    return true;
+}
+
+QList<IndexV2> refreshBlockCodeIndex(AsmMemBlock& block){
+    QList<IndexV2> index;
+    for(int i = 0;i<block.codes.length();i++){
+        for(int j = 0;j<block.codes[i].marks.length();j++){
+            index.append({i,j});
+        }
+    }
+    return index;
+}
+QStringList montageBlockCodeMarksDef(AsmMemBlock& block){
+    QStringList memBlockMarksDef;
+    for(int i = 0;i<block.codes.length();i++){
+        memBlockMarksDef.append(block.codes[i].marks);
+    }
+    return memBlockMarksDef;
+}
+//判断是否存在重名定义的标记
+bool detectDuplicateDefMark(AsmSrc & asmSrc,QStringList*promptText){
+    bool isDuplicate = false;
+    //已单个内存块为判断域
+    for(int k = 0;k<asmSrc.memBlocks.length();k++){
+        AsmMemBlock&thisBlock = asmSrc.memBlocks[k];
+        QStringList marks = montageBlockCodeMarksDef(thisBlock);
+        QList<IndexV2> index = refreshBlockCodeIndex(thisBlock);
+        //index[0]所属的子指令索引号，index[0]子指令中标记的索引号
+
+        QList<Prompt> pormpts;
+        for(int i = 0;i<marks.length();i++){
+            //排查当前内存块之前的所有内存块，是否有与当前内存块命名重复的。如果有就无需排查了，避免产生重复的报错信息
+            bool isHaveRepeat = 0;
+            for(int j = 0;j<i;j++){
+                if(marks[j]==marks[i]){
+                    isHaveRepeat = 1;
+                    break;
+                }
+            }
+            if(isHaveRepeat){
+                continue;
+            }
+
+            QList<int> repeat;
+            //从当前标记后一个标记开始排查命名与当前标记命名相同的标记，并将索引号存入repeat
+            for(int j = i+1;j<marks.length();j++){
+                if(marks[j]==marks[i]){
+                    repeat.append(j);
+                }
+            }
+
+            if(repeat.length()!=0){
+                //如果存在重复，发出报错信息
+                Prompt prpt;
+                prpt.isError = 1;//报错:内存块/标记命名不符合规范
+
+                prpt.content = "There are "+QString::number(repeat.length()+1)+ " label with duplicate names pointing to different addresses";
+
+                prpt.append(asmSrc.path,asmSrc.asmSrcText,thisBlock.codes[index[i].index1].startLine,thisBlock.codes[index[i].index1].endLine);
+                for(int j = 0;j<repeat.length();j++){
+                    prpt.append(asmSrc.path,asmSrc.asmSrcText,thisBlock.codes[index[repeat[j]].index1].startLine,thisBlock.codes[index[repeat[j]].index1].endLine);
+                }
+                pormpts.append(prpt);
+            }
+        }
+        if(pormpts.length()>0){
+            for(int i = 0;i<pormpts.length();i++){
+                QString txt = grenratePromptText(pormpts[i]);
+                promptText->append(txt);
+            }
+            isDuplicate |= true;
+        }
+    }
+    return isDuplicate;
+}
+
+
+
+}}
+QT_END_NAMESPACE
---
+++ OpenQinling-023A/023A编译工具链/编译工具链源码/OpenQinling_ASM_BinaryObjectLib.h
@@ -0,0 +1,101 @@
+﻿#ifndef OPENQINLING_ASM_BINARYOBJECTLIB_H
+#define OPENQINLING_ASM_BINARYOBJECTLIB_H
+#include <QString>
+#include <QStringList>
+#include <QDataStream>
+#include <QBuffer>
+#include <QDebug>
+#include "OpenQinling_ASM_FormatText.h"
+#pragma execution_character_set("utf-8")
+QT_BEGIN_NAMESPACE
+namespace OpenQinling {
+namespace ASM_Compiler{
+
+//定义的地址标记
+class BinaryMark{
+public:
+    QString markName;//标记名
+    QString affiBlockName;//所属的内存块名
+    uint affBlockOffset = 0;//相较于所属内存块基址的偏移地址(字节-左低右高)
+    QString toString();
+    QString toName();
+};
+//外部引入的地址标记
+class BinaryImportMark{
+public:
+    QString markName;//标记名
+    QString affiBlockName;//所属的内存块名
+    QString toString();
+    QString toName();
+};
+//地址标记的引用
+class BinaryQuoteMark{
+public:
+    bool isImport;//是否是从其他原文件引入的
+    QString name;//标记名[如果为为空，就是指向内存块首地址的标记]
+    QString affiBlockName;//定义该标记的内存块名
+    //使用该地址标记的内存块偏移地址 第offset_Byte字节第offset_Bit位开始
+    uint order_offset_Byte;//使用该地址标记的内存块偏移地址(字节-左低右高)
+    uint order_offset_Bit;//使用该地址标记的内存块偏移地址(比特-左低右高)
+
+    //32位cpu是31-0之间。例如将该地址标记的低16位用在 mov r1,[内存块.标记] 中，higth=15,low=0
+    uint higth,low;//使用了该地址标记的哪几位(比特-左高右低)
+    int offset = 0;//标记所指向的内存地址的偏移值
+    uint value = 0;//标记指向的绝对内存地址
+
+    QString toString();
+    QString toName();
+
+
+};
+//生成的二进制内存块
+class BinaryMemBlock{
+public:
+    QString name;//内存块名
+    bool isExport;//是否导出
+    //isConstData与isZIData互斥，不可能同时为真
+    bool isConstData;//是否为常量数据块(指令内存块无效)
+    bool isZIData;//是否为非初始化数据块(指令内存块无效)
+
+    bool type;//内存块的类型
+    uint baseAdd;//内存块的基地址[在编译器中该参数无用，用在链接器中]
+    QList<BinaryMark> marks;//内存块定义的标记
+    QList<BinaryQuoteMark> markQuotes;//当前内存块引用的标记[如果是非初始化数据块，则必然为空]
+
+    uint binLength;//内存块中二进制数据的字节数
+    QByteArray bin;//内存块的二进制数据[如果是非初始化数据块，则为空]
+    QString toString();
+
+};
+
+//当前汇编源文件最终编译生成的类型
+class BinaryObject{
+public:
+    QList<BinaryImportMark> marks_import;//导入外部源文件的标记
+    QList<BinaryMemBlock> blocks;//内存块
+    QString toString();
+
+
+    //将BinaryObject转为格式化的二进制数据
+    QByteArray toFormatByteArray();
+    //从格式化的二进制数据中解析出BinaryObject对象[isSuceess是否解析成功]
+    static BinaryObject fromFormatByteArray(QByteArray &bin,bool *isSuceess);
+    //将obj列表转为二进制
+    static QByteArray objsToFormatByteArray(QList<BinaryObject> objs);
+    //从二进制列表中解析出obj列表
+    static QList<BinaryObject> objsfromFormatByteArray(QByteArray &bin,bool *isSuceess);
+
+};
+
+class LibraryFile{
+public:
+    QString fileName;
+    QList<BinaryObject> libData;
+};
+
+//判断是否有库文件之间存在公开内存块重名
+bool judgeHaveLibBlockDupName(QList<LibraryFile> &libfiles,QStringList*prompts);
+
+}}
+QT_END_NAMESPACE
+#endif // OPENQINLING_ASM_BINARYOBJECTLIB_H
